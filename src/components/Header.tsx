7f48d312aa3a99ca20dd89e647aa70297ca30ef5
Hardik0102 <hardik1104j@gmail.com>
2024-01-27 13:26:27 +0530

    first comment

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+import { AppBar, Toolbar } from '@mui/material';
+
+const Header: React.FC = ()  => {
+
+    return(
+        <AppBar>
+            <Toolbar>
+                Todo 
+            </Toolbar>
+        </AppBar>
+    );
+
+};
+export default Header;
